70237fc9d4b0da1ce479c990151a6eb48bb5f47f
Josh Bowen <josh.bowen@capgemini.com>
2019-10-30 12:57:12 +0000

    Add nonempty option only when needed

--- s3fs/run.sh
+++ s3fs/run.sh
@@ -3,4 +3,10 @@
 echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > /etc/passwd-s3fs
 chmod 0400 /etc/passwd-s3fs
 
+if [ -z "$(ls -A $MNT_POINT)" ]; then
+     S3_EXTRAVARS=''
+else
+    S3_EXTRAVARS=',nonempty'
+fi
+
 /usr/local/bin/s3fs $S3_BUCKET $MNT_POINT -f -o url=${S3_ENDPOINT},allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10${S3_EXTRAVARS}
